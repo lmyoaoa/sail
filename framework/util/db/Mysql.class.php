ed108362d2d066b71cf3e8e7c6833830143d8068
lmyoaoa <lmyoaoa@gmail.com>
2014-06-29 18:16:32 +0800

    mysql基类恢复为非静态类

--- framework/util/db/Mysql.class.php
+++ framework/util/db/Mysql.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * mysql操作基类，静态类，多次使用场景下效率更高
+ * mysql操作基类
  * @author 李明友
  * @since 2014-06-20
  */
@@ -8,62 +8,58 @@
 class Mysql {
 
     //是否长连接，默认false
-    public static $pconnect = false;
+    public $pconnect = false;
 
     //是否开启debug模式
-    public static $debug = false;
+    public $debug = false;
 
-    protected static $dbConfig;
-    protected static $dbName;
-    protected static $tableName;
+    protected $host;
+    protected $port;
+    protected $dbName;
+    protected $dbUser;
+    protected $dbPasswd;
 
     //pdo object
-    protected static $_conn;
+    protected $_conn;
 
-    protected static $_pcon;
+    protected $_pcon;
 
     /**
-     * 查询数据表
-     */
-    public static function getRows() {
-        $conn = self::getConnect();
-        $sql = 'select * from ' . self::$tableName;
-        echo $sql;
-        var_dump($conn);
-    }
-
-    public static function getConnect() {
-        if( self::$_conn ) {
-            return self::$_conn;
-        }
-        return self::connect(self::$dbConfig, self::$dbName);
-    }
-
-     /**
      * 数据库初始化
      * @param array $dbValues
      */
-    private static function connect($dbConfig, $dbName) {
-        self::$dbConfig = $dbConfig;
-        self::$dbName = $dbName;
+    function __construct($dbConfig, $dbName) {
 
-/*
         $this->host     = $dbConfig['host'];
         $this->port     = $dbConfig['port'];
         $this->dbUser   = $dbConfig['dbUser'];
         $this->dbPasswd = $dbConfig['dbPasswd'];
 
         $this->dbName   = $dbName;
-        */
+    }
 
+    /**
+     * 查询数据表
+     */
+    public function getRows() {
+        $conn = $getConnect();
+        var_dump($conn);
+    }
+
+    public function getConnect() {
+        if( $this->_conn ) {
+            return $this->_conn;
+        }
+        return $this->connect();
+    }
+    
+    private function connect() {
         try {
-            self::$_pcon = array(PDO::ATTR_PERSISTENT => self::$pconnect);
-            self::$_conn = new PDO(
-                'mysql:host=' . self::$dbConfig['host'] . ';port=' . self::$dbConfig['port'] . 
-                ';dbname=' . self::$dbConfig['dbName'] . ';', 
-                self::$dbConfig['dbUser'],
-                self::$dbConfig['dbPasswd']);
-            return self::$_conn;
+            $this->_pcon = array(PDO::ATTR_PERSISTENT => $this->pconnect);
+            $this->_conn = new PDO("mysql:host={$this->host};port={$this->port};dbname={$this->dbName};", 
+                $this->dbUser,
+                $this->dbPasswd);
+            var_dump($this->_conn);
         }catch( Exception $e ) {
             echo $e->getMessage();
         }
